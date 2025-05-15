2a8648c5c0b6886b0a98a1f49f0b9723a47293ff
onoja123 <okpeonoja18@gmail.com>
2025-05-15 17:22:40 +0100

    Feat: worked on the loader for the dashbaord

--- app/dashboard/product/view-product/page.tsx
+++ app/dashboard/product/view-product/page.tsx
@@ -74,7 +74,13 @@ export default function AddUserPage() {
   ];
 
   if (isLoading) {
-    return <p>Loading....</p>;
+    return (
+  <div className="flex flex-col items-center justify-center h-screen space-y-4">
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <p className="text-gray-600 text-sm">Fetching your dashboard...</p>
+  </div>
+);
+
   }
 
   if (error) {
--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -59,7 +59,13 @@ export default function DemoPage() {
   );
 
   if (isLoading || loadingUser) {
-    return <p>Loading....</p>;
+    return (
+  <div className="flex flex-col items-center justify-center h-screen space-y-4">
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <p className="text-gray-600 text-sm">Fetching your dashboard...</p>
+  </div>
+);
+
   }
   // console.log(error?.response?.data?.error, "jjjjjjj");
 
